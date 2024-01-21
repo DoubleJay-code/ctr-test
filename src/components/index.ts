7ef0a991f86cd80a31b4b303479ac0b5df17f273
Artem <makarov@qtim.ru>
2024-01-21 17:03:43 +0600

    add svg

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,12 +2,14 @@ import UiSvg from '@/components/ui/UiSvg.vue'
 import UiSelect from '@/components/ui/UiSelect.vue'
 import UiDropdown from '@/components/ui/UiDropdown.vue'
 import UiButton from '@/components/ui/UiButton.vue'
+import NotificationItem from '@/components/notification/NotificationItem.vue'
 
 const components = [
 	{ name: 'ui-svg', component: UiSvg },
 	{ name: 'ui-select', component: UiSelect },
 	{ name: 'ui-dropdown', component: UiDropdown },
-	{ name: 'ui-button', component: UiButton }
+	{ name: 'ui-button', component: UiButton },
+	{ name: 'notification-item', component: NotificationItem }
 ]
 
 export default components
